55e5055477d9039b58671824ddcafb90393d0810
Like6po <some@gmail.com>
2022-08-18 15:52:27 +0300

    🚀: Добавил модели для админки

--- books_site/books/admin.py
+++ books_site/books/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Author, Book, Comment
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["id", "full_name", "get_books_count", "get_comments_count"]
+
+    def get_books_count(self, obj):
+        return Book.objects.filter(authors=obj.id).count()
+
+    get_books_count.short_description = "Количество книг"
+
+    def get_comments_count(self, obj):
+        return Comment.objects.filter(author=obj.id).count()
+
+    get_comments_count.short_description = "Количество комментариев"
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "publish_date", "get_authors", "get_comments_count"]
+
+    def get_authors(self, obj):
+        return ", ".join([p.first_name for p in obj.authors.all()])
+
+    get_authors.short_description = "Авторы"
+
+    def get_comments_count(self, obj):
+        return Comment.objects.filter(book=obj.id).count()
+
+    get_comments_count.short_description = "Количество комментариев"
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
